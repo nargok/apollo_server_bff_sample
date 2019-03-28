10981f6d100684c5a2ebbe16ee57ebbf39f7bbb3
Ryoichi Gokan <ryoichi.gokan@compass-e.com>
2019-03-28 17:54:37 +0900

    Created src/index.js

--- src/index.js
+++ src/index.js
@@ -5,13 +5,37 @@ import { ApolloServer, gql } from 'apollo-server-express';
 const app = express();
 
 // schemaをつくる
+const schema = gql`
+  type Query {
+    me: User
+  }
+  
+  type User {
+    username: String!
+  }
+`;
 
-// resolverをつくる
+// resolverをつくる こっちはObject
+const resolvers = {
+    Query: {
+        me: () => {
+            return {
+                username: '百獣団長クロコダイン'
+            };
+        },
+    },
+};
 
 // ApolloServerの設定をする
-
+const server = new ApolloServer({
+    typeDefs: schema,
+    resolvers,
+})
 
 // Middleware設定をする graphqlをエンドポイントに付与する
-
+server.applyMiddleware({ app, path: '/graphql' });
 
 // サーバー起動の設定をする
+app.listen({ port: 8080 }, () => {
+   console.log('Apollo Server on http://localhost:8080/graphql');
+});
