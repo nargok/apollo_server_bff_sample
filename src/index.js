39070a62807ce9b060219d7a7271c476140a7b2b
Ryoichi Gokan <ryoichi.gokan@compass-e.com>
2019-03-28 17:48:57 +0900

    src/index.jsに擬似コードを書く

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import { ApolloServer, gql } from 'apollo-server-express';
+
+// appに向かっていろいろ設定する
+const app = express();
+
+// schemaをつくる
+
+// resolverをつくる
+
+// ApolloServerの設定をする
+
+
+// Middleware設定をする graphqlをエンドポイントに付与する
+
+
+// サーバー起動の設定をする
