4d0df86a6d9eca8e718db3cd4b2df0c0388e458d
nargok <nargok@gmail.com>
2019-04-01 22:17:46 +0900

    Added Description to createJsonUser mutation

--- src/schema/jsonPlaceholder.js
+++ src/schema/jsonPlaceholder.js
@@ -2,6 +2,9 @@ import { gql } from 'apollo-server-express';
 
 export default gql`
   extend type Mutation {
+    """
+    外部サービス(JSONPlaceholder)にユーザ情報をPOSTする
+    """
     createJsonUser(name: String!, age: Int!): JsonUser
   }
   
